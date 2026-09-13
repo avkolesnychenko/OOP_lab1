6c7274deb40f97961156b11299cd1a42c3b09c36
avkolesnychenko <a.v.kolesnychenko@student.khai.edu>
2026-09-13 16:06:59 +0300

    Add project files.

---
+++ lab1/CrustType.cs
@@ -0,0 +1,10 @@
+﻿namespace PizzaAsteroidApp
+{
+   public enum CrustType
+    {
+        Thin = 1,
+        CheeseStuffed,
+        DeepDish,
+        Classic
+    }
+}
---
+++ lab1/PizzaAsteroid.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace PizzaAsteroidApp
+{
+    public class PizzaAteroid
+    {
+        public string Name;
+        public CrustType Crust;
+        public double DiameterKm;
+        public int TemperatureCelsius;
+        public bool HasExtraCheese;
+
+        private DateTime discoveryDate;
+
+        public void SetDiscoveryDate(DateTime date)
+        {
+            discoveryDate = date;
+        }
+
+        public DateTime GetDiscoveryDate()
+        {
+            return discoveryDate;
+        }
+
+        public PizzaAteroid(string name, CrustType crust, double diameterKm, int temperatureCelsius, bool hasExtraCheese, DateTime discoveryDate)
+        {
+            Name = name;
+            Crust = crust;
+            DiameterKm = diameterKm;
+            TemperatureCelsius = temperatureCelsius;
+            HasExtraCheese = hasExtraCheese;
+            this.discoveryDate = discoveryDate;
+        }
+
+        public void HeatUp(int temperature)
+        {
+            TemperatureCelsius += temperature;
+            Console.WriteLine($"Астероїд '{Name}' нагрівся на {temperature}°C. Поточна температура: {TemperatureCelsius}°C.");
+            if (TemperatureCelsius > 60 && HasExtraCheese)
+            {
+                Console.WriteLine("УВАГА! Сир на поверхні розплавився і утворив захисну мантію-скоринку!");
+            }
+        }
+
+        public void Slice(int slices)
+        {
+            if (slices <= 1)
+            {
+                Console.WriteLine($"[!] Астероїд '{Name}' неможливо нарізати на {slices} шматочків.");
+                return;
+            }
+            DiameterKm /= Math.Sqrt(slices);
+            Console.WriteLine($"[!] Астероїд '{Name}' розділено на {slices} шматків! Новий діаметр кожного шматка: {DiameterKm:F2} км.");
+        }
+
+        public string CollideWithTarget(string targetName)
+        {
+            return $"УВАГА! ЗІТКНЕННЯ!!! '{Name}' врізається в об'єкт '{targetName}', покриваючи його товстим шаром соусу, астероїд розлітається на обломки типу {Crust}!";
+        }
+    }
+}
